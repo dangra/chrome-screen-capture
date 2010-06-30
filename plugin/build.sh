fffead5c3df08474450eee35d572008b62da0e4e
jingzhao@google.com <jingzhao@google.com@72d34acd-96ab-fc25-247f-fc063804e312>
2010-06-30 04:58:05 +0000

    git-svn-id: http://chrome-screen-capture.googlecode.com/svn/trunk@47 72d34acd-96ab-fc25-247f-fc063804e312

--- plugin/build.sh
+++ plugin/build.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-SDK=~/npn/xulrunner-sdk
-
-gcc -O2 -fPIC -DGTK -Wno-write-strings -I$SDK/include `pkg-config --cflags --libs gtk+-2.0` -DXPCOM_GLUE -DMOZILLA_STRICT_API -shared -o npcapture.so npcapture.cpp
+gcc -O2 -fPIC -DGTK -Wno-write-strings `pkg-config --cflags --libs gtk+-2.0` -shared -o npcapture.so npcapture.cpp
