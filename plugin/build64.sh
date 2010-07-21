79931453e20a0d43c68dc3153ea7bb77fc8e81bb
jingzhao@google.com <jingzhao@google.com@72d34acd-96ab-fc25-247f-fc063804e312>
2010-07-21 14:06:32 +0000

    Update Linux NPAPI.
    git-svn-id: http://chrome-screen-capture.googlecode.com/svn/trunk@73 72d34acd-96ab-fc25-247f-fc063804e312

--- plugin/build64.sh
+++ plugin/build64.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 gcc -O2 -fPIC -DGTK -Wno-write-strings `pkg-config --cflags --libs gtk+-2.0` \
-  -shared -o npcapture.so npcapture.cpp save.cpp
+  -shared -o npcapture64.so npcapture.cpp save.cpp
