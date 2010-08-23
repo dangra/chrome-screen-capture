37910af22d2aceb780e4164638ecea27599250e8
wangxianzhu@google.com <wangxianzhu@google.com@72d34acd-96ab-fc25-247f-fc063804e312>
2010-08-23 03:40:05 +0000

    Automatically strip symbols.
    git-svn-id: http://chrome-screen-capture.googlecode.com/svn/branches/linux-auto-save-path@169 72d34acd-96ab-fc25-247f-fc063804e312

--- plugin/build.sh
+++ plugin/build.sh
@@ -2,8 +2,9 @@
 if [ "$1" == "debug" ]; then
   FLAG=-g
 else
-  FLAG=-O2
+  FLAG="-O2 -Xlinker --strip-all"
 fi
 
 gcc $FLAG -m32 -fPIC -DGTK -Wno-write-strings `pkg-config --cflags --libs gtk+-2.0` \
   -shared -o npcapture.so npcapture.cpp save.cpp plugin.cpp np_entry.cpp
+
