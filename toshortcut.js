a97fc8ad401a289711e9543f8bddfbb0cb2662e4
ye_feng@vanceinfo.com <ye_feng@vanceinfo.com@72d34acd-96ab-fc25-247f-fc063804e312>
2010-12-27 05:38:14 +0000

    规范代码
    git-svn-id: http://chrome-screen-capture.googlecode.com/svn/branches/shortcuts-support@262 72d34acd-96ab-fc25-247f-fc063804e312

--- toshortcut.js
+++ toshortcut.js
@@ -1,24 +1,24 @@
 var toshortcut = { 
 
-    init: function() {
-      document.body.onkeydown = toshortcut.doshortcut;
-    },
+  init: function() {
+    document.body.onkeydown = toshortcut.doshortcut;
+  },
 
-    doshortcut: function (event) {        
-      if (event.ctrlKey && event.altKey) {
-        if(window.event.keyCode == 82) {
-          toshortcut.sendMessage({msg: 'capture_area'});
-        } else if(window.event.keyCode==86) {
-          toshortcut.sendMessage({msg: 'capture_window'});       
-        } else if(window.event.keyCode ==72) {
-          toshortcut.sendMessage({msg: 'capture_webpage'});
-        }     
-      }
-    },
+  doshortcut: function (event) {        
+    if (event.ctrlKey && event.altKey) {
+      if(window.event.keyCode == 82) {
+        toshortcut.sendMessage({msg: 'capture_area'});
+      } else if(window.event.keyCode==86) {
+        toshortcut.sendMessage({msg: 'capture_window'});       
+      } else if(window.event.keyCode ==72) {
+        toshortcut.sendMessage({msg: 'capture_webpage'});
+      }     
+    }
+  },
 
-    sendMessage: function(message) {
-      chrome.extension.sendRequest(message);
-    } 
+  sendMessage: function(message) {
+    chrome.extension.sendRequest(message);
+  } 
 };
 
 toshortcut.init();
