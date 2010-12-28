8f30b54da0788e221690d3377a8281ace4c9e401
ye_feng@vanceinfo.com <ye_feng@vanceinfo.com@72d34acd-96ab-fc25-247f-fc063804e312>
2010-12-28 10:15:03 +0000

    修改快捷键
    git-svn-id: http://chrome-screen-capture.googlecode.com/svn/branches/shortcuts-support@281 72d34acd-96ab-fc25-247f-fc063804e312

--- shortcut.js
+++ shortcut.js
@@ -6,11 +6,11 @@ var toshortcut = {
 
   doshortcut: function (event) {
     if (event.ctrlKey && event.altKey) {
-      if(window.event.keyCode == 82) {
+      if(window.event.keyCode == 67) {
         toshortcut.sendMessage({msg: 'capture_area'});
       } else if(window.event.keyCode == 86) {
         toshortcut.sendMessage({msg: 'capture_window'});       
-      } else if(window.event.keyCode == 72) {
+      } else if(window.event.keyCode == 88) {
         toshortcut.sendMessage({msg: 'capture_webpage'});
       }     
     }
