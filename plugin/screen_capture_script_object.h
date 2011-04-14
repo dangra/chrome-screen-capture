08889d5bf8a5ae37f9f2e52f2b7bebf4a2a24d77
zhang_jikai@vanceinfo.com <zhang_jikai@vanceinfo.com@72d34acd-96ab-fc25-247f-fc063804e312>
2011-04-14 05:56:13 +0000

    support print feature on windows platform
    Review URL: http://codereview.appspot.com/4376048
    
    git-svn-id: http://chrome-screen-capture.googlecode.com/svn/branches/support-print@351 72d34acd-96ab-fc25-247f-fc063804e312

--- plugin/screen_capture_script_object.h
+++ plugin/screen_capture_script_object.h
@@ -33,10 +33,15 @@ public:
                       NPVariant* result);
   bool SaveToClipboard(const NPVariant* args, uint32_t argCount,
                        NPVariant* result);
+  bool PrintImage(const NPVariant* args, uint32_t argCount, NPVariant* result);
 
   void InitHandler();
 
 private:
+  enum ImageType {
+    kImageTypePNG = 0,
+    kImageTypeJPEG
+  };
   static bool SaveFile(const char* fileName, const unsigned char* bytes,
                        int byteLength);
   static bool SaveFileBase64(const char* fileName, const char* base64,
@@ -45,7 +50,9 @@ private:
   static void InvokeCallback(NPP npp, NPObject* callback, const char* param);
   static void InvokeCallback(NPP npp, NPObject* callback, bool param);
 #ifdef _WINDOWS
-
+  bool PrintImageWin(HDC printer_dc, ImageType imagetype, 
+                     unsigned char* imagedata, int imagelen, 
+                     int width, int height);
   std::string GetPicturePath();
 #elif defined GTK
 
